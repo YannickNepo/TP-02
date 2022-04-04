c1725f185d73e732dbb51e87683b5057afeb1faa
YannickNepo <101335474+YannickNepo@users.noreply.github.com>
2022-04-04 12:10:26 -0300

    orimer ejercicio

---
+++ Assets/MayoriaDeEdad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadusuario;
+    public Text miTexto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            miTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+    
+
+    
+}
