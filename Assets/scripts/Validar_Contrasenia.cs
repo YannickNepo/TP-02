27bdd5d4bd3555ae399a30c62f895ff59690eb8d
YannickNepo <101335474+YannickNepo@users.noreply.github.com>
2022-04-19 13:58:00 -0300

    contraseña ejercicio

---
+++ Assets/scripts/Validar_Contrasenia.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Validar_Contrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMensaje;
+    public GameObject cartelitoMensaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelitoMensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if(contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelitoMensaje.SetActive(true);
+            textoMensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMensaje.SetActive(true);
+            textoMensaje.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
